8beefa007020098e22af5fb4c8eccd5fcdad79d5
Mako <10919093+tex2e@users.noreply.github.com>
2024-11-02 11:29:42 +0900

    update

--- Parser/WindowsBatchParser.cs
+++ Parser/WindowsBatchParser.cs
@@ -18,25 +18,24 @@ public static class WindowsBatchParser
     /// 単行コメントの構文
     /// </summary>
     public static Parser<string> commentRule =
-        (from directive in Parse.IgnoreCase("REM").Text()
+        from directive in Parse.IgnoreCase("REM").Text()
         from spaces in Parse.WhiteSpace
         from comment in Parse.CharExcept("\r\n").Many().Text()
         from newline in Parse.LineTerminator
-        select comment).Named("comment");
+        select comment;
 
     /// <summary>
     /// 複数行コメントの構文
     /// </summary>
     public static Parser<Comment> commentsRule =
-        (from comments in commentRule.Many()
-        select new Comment(string.Join("\n", comments))).Named("comments");
+        from comments in commentRule.Many()
+        select new Comment(string.Join("\n", comments));
 
     /// <summary>
     /// GOTO用のラベルの構文
     /// </summary>
     public static Parser<Label> labelRule =
-        (from leading in Parse.WhiteSpace.Many()
-        from mark in Parse.Char(':')
+        (from mark in Parse.Char(':')
         from label in identifierRule
         select new Label(label)).Named("label");
 
@@ -44,12 +43,12 @@ public static class WindowsBatchParser
     /// 変数への値代入の構文
     /// </summary>
     public static Parser<SetVariable> setVariableRule =
-        (from set in Parse.IgnoreCase("SET")
+        from set in Parse.IgnoreCase("SET")
         from _ in Parse.WhiteSpace.AtLeastOnce()
         from name in identifierRule
         from eq in Parse.Char('=')
         from value in Parse.CharExcept("\r\n").Many().Text()
-        select new SetVariable(name, value)).Named("setvariable");
+        select new SetVariable(name, value);
 
     /// <summary>
     /// ステートメントの構文
@@ -63,6 +62,6 @@ public static class WindowsBatchParser
     /// バッチファイルの構文
     /// </summary>
     public static readonly Parser<BatchFile> BatchFile =
-        from statements in statementRule.Many().End()
+        from statements in statementRule.XMany().End()
         select new BatchFile(statements);
 }
